98d3c9f9b9408148e7d709cfdacc90e2c0179a2a
Johana López Pinto <johislp4@gmail.com>
2019-03-09 16:58:54 -0500

    Simple Array Sum

---
+++ ejercicio1.php
@@ -0,0 +1,13 @@
+<?php
+
+/*Complete the simpleArraySum function in the editor below. It must return the sum of the array elements as an integer.
+
+simpleArraySum has the following parameter(s):
+
+ar: an array of integers*/
+
+function simpleArraySum($ar) {
+
+    return array_sum($ar);
+
+}
